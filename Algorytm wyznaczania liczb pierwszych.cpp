69003fa9216249e2aafe156cdbc5c342c26d1b2a
RAKU1337 <kryrak2@gmail.com>
2022-03-01 11:05:09 +0100

    Komentarz

---
+++ Algorytm wyznaczania liczb pierwszych.cpp
@@ -0,0 +1,23 @@
+﻿// Algorytm wyznaczania liczb pierwszych.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int jednostka;
+	cout << "Podaj wartość" << endl; 
+	cin >> jednostka
+		for (int i = 2; jednostka % i == 0; i++) {
+			int wynik = jednostka & i;
+			if (wynik != 0) {
+
+			}
+			if (wynik == 0)
+				cout << jednostka << "jest liczbą pierwszą";
+			break;
+
+}
+
